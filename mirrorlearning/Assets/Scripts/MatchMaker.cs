0e26beec796b4e5e182a9363899f52919751be83
blueligh <hangaram2@itsnowball.com>
2024-05-21 19:38:08 +0900

    lobby study ep2-2

--- mirrorlearning/Assets/Scripts/MatchMaker.cs
+++ mirrorlearning/Assets/Scripts/MatchMaker.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+using IO.Swagger.Model;
 
-public class MatchMaker : MonoBehaviour
+namespace MirrorBasics
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Match
     {
-        
+        public string matchID;
+        public SyncListGameObject players = new SyncListGameObject();
+        public Match(string matchID, GameObject player)
+        {
+            this.matchID = matchID;
+            players.Add(player);
+        }
+
+        public Match()
+        {
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    public class SyncListGameObject : SyncList<GameObject> { }
+
+    [System.Serializable]
+    public class SyncListMatch : SyncList<Match> { }
+
+    public class MatchMaker : NetworkBehaviour
     {
-        
+        public static MatchMaker instance;
+
+        public SyncListMatch matches = new SyncListMatch();
+        public SyncList<string> matchIDs = new SyncList<string>();
+
+        private void Start()
+        {
+            instance = this;
+        }
+        public bool HostGame(string _matchID, GameObject _player)
+        {
+            if (!matchIDs.Contains(_matchID))
+            {
+                matchIDs.Add(_matchID);
+                matches.Add(new Match(_matchID, _player));
+                Debug.Log($"match generated");
+                return true;
+            }
+            else
+            {
+                Debug.Log($"match id already exists");
+                return false;
+            }
+        }
+
+        internal static string GetRandomMatchID()
+        {
+            string _id = string.Empty;
+            for (int i = 0; i < 5; i++)
+            {
+                int random = Random.Range(0, 36);
+                if (random < 26)
+                {
+                    _id += (char)(random + 65);
+                }
+                else
+                {
+                    _id += (random + 26).ToString();
+                }
+            }
+            Debug.Log($"Random Match ID: {_id}");
+            return _id;
+        }
     }
+
 }
--- mirrorlearning/Assets/Scripts/Player.cs
+++ mirrorlearning/Assets/Scripts/Player.cs
@@ -9,13 +9,33 @@ namespace MirrorBasics
     {
         public static Player localPlayer;
 
-        private void Start() {
-            if(isLocalPlayer){
+        private void Start()
+        {
+            if (isLocalPlayer)
+            {
                 localPlayer = this;
             }
         }
-        public void HostGame(){
-            
+        public void HostGame()
+        {
+            string matchID = MatchMaker.GetRandomMatchID();
+            CmdHostGame(matchID);
+
+        }
+
+        [Command]
+        void CmdHostGame(string _matchID)
+        {
+            if (MatchMaker.instance.HostGame(_matchID, gameObject))
+            {
+                Debug.Log($"<color=green>game hosted successfully</color>");
+                
+
+            }
+            else
+            {
+                Debug.Log($"<color=red>game hosted failed</color> ");
+            }
         }
     }
 }
